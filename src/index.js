6ab04ce53f3d520566af76ea086d0c3ac8996282
Reinis Riekstins <rr14022@lu.lv>
2016-12-30 16:56:02 +0200

    refectored accordions

--- src/index.js
+++ src/index.js
@@ -6,8 +6,8 @@ import './styles/foundation.css'
 
 import { store, actions } from './store'
 import Accordion from './components/Accordion'
-import LogInputRow from './components/LogInputRow'
-import Panel from './components/Panel'
+import AccordContent1 from './components/AccordContent1'
+import AccordContent2 from './components/AccordContent2'
 
 // for using in browser console
 window.store = store
@@ -15,10 +15,11 @@ window.actions = actions
 
 render(
   <div className="accordion-wrapper">
-    <Accordion store={ store } actions={ actions }>
-      <LogInputRow store={ store } actions={ actions } />
-      <LogInputRow store={ store } actions={ actions } />
-      <Panel store={ store } actions={ actions } />
+    <Accordion>
+      <AccordContent1 store={ store } actions={ actions } />
+    </Accordion>
+    <Accordion>
+      <AccordContent2 store={ store } actions={ actions } />
     </Accordion>
     <DevTools />
   </div>,
