9e5e1d2c4f198b0186c679ef48e3358a0e29dfc6
Reinis Riekstins <rr14022@lu.lv>
2017-01-14 20:35:41 +0200

    added react-drag-select-table imports

--- src/components/AccordContent2.js
+++ src/components/AccordContent2.js
@@ -1,47 +1,22 @@
 import React from 'react'
 import { observer } from 'mobx-react'
-
-import Table from './dragSelectTable/Table'
-import TableBody from './dragSelectTable/TableBody'
-import TableHead from './dragSelectTable/TableHead'
+import {
+  Tbody,
+  Tr,
+  SelectAllVisible,
+  DeselectAll,
+  Selected,
+  Filter
+ } from 'react-drag-select-table'
 
 const AccordContent2 = ({ store, actions }) => {
-  const { logs } = store.playerSelectPanel
+  const { logs } = store.playerSelect
 
-  const createPlayerTable = logs => {
-    return (
-      <div className="row">
-        <div className="large-5 columns">
-          <h3>Unselected Players</h3>
-          <table>
-            <TableHead />
-            <TableBody store={ store } actions={ actions } />
-          </table>
-        </div>
-        <div className="large-2 columns" style={{ textAlign: 'center' }}>
-          <button className="button secondary">
-            <i
-              className="material-icons"
-              style={{fontSize:'2em'}}>
-              swap_horiz
-              {/* if viewport width is small enough, use swap_vert */}
-            </i>
-          </button>
-        </div>
-        <div className="large-5 columns">
-          <h3>Selected Players</h3>
-          <Table>
-            <Table.Head />
-            <Table.Body store={ store } actions={ actions } />
-          </Table>
-        </div>
-      </div>
-    )
-  }
+
 
   return (
     <div className="accordionContent" >
-      { logs.length === 0 ? '' : createPlayerTable(logs) }
+
     </div>
   )
 }
